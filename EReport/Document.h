785effe1e0660467664cbbf6bb3236b2a0d695ce
weitek <weitek.mvl@gmail.com>
2012-11-29 16:53:37 +0400

    Initial Commit

---
+++ EReport/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  EReport
+//
+//  Created by Victor Mylcin on 29.11.12.
+//  Copyright (c) 2012 Victor Mylcin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ EReport/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  EReport
+//
+//  Created by Victor Mylcin on 29.11.12.
+//  Copyright (c) 2012 Victor Mylcin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
